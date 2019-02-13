5d370ed7badc05c3954760f75426ec1d2b8dc77d
Albina <darkcrashma@mail.ru>
2019-02-13 13:30:24 +0800

    #add: Добавлена галерея, слайдер и партнеры компании

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+    $('.article-slider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoPlay: 5000
+    });
+
+});
