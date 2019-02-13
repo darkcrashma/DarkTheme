ec3fb46023f6790f02e2b8904b13faa57ee07965
Антон Прокопьев <inpokon@gmail.com>
2019-02-13 21:25:34 +0800

    #fix: Исправлены мелкие косяки в верстке, стилизованы стрелки, добавлены псевдоэлементы, сверстан и прибит футер, исправлены ховеры

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready( function() {
     $('.article-slider').slick({
-        arrows: false,
+        arrows: true,
         dots: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoPlay: 5000
+        autoPlay: 5000,
+        nextArrow: '<div class="article-slider__arrow article-slider__arrow--next"><svg><use xlink:href="img/sprites.svg#next"></use></svg></div>', // Кастомная стрелка "далее"
+        prevArrow: '<div class="article-slider__arrow article-slider__arrow--prev"><svg><use xlink:href="img/sprites.svg#prev"></use></svg></div>'
     });
 
 });
